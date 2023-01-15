6aa1df8fa6c354f8226fa360d1cd3e6db0c30757
Enrique Rodriguez <enrique.rodriguez9@upr.edu>
2023-01-14 22:50:47 -0400

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='sqlmin',
     version='1.0.0',
     description='SQL Minifier',
+    long_description="SQL Minifier",
     author='Enrique Rodriguez',
     author_email='rodriguez.enrique.pr@gmail.com',
     entry_points={
